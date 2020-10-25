25eb036628bf6a6ec6dba7d4d7aa66650ef1ec95
Akbeta <34417875+Akbeta@users.noreply.github.com>
2020-10-25 15:13:12 +0300

    select card function and default card animation added

--- src/components/GameCards.vue
+++ src/components/GameCards.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="game-area">
+    <p>{{answer}}</p>
     <h1 class='title'>Find <span>the correct</span> <strong>movie ?</strong> </h1>
     <h4 class="desc">Select one movie card and then click the question mark</h4>
     <div class="container">
       <transition-group name="rotate-all" appear class="card-container">
-        <Card
+        <app-card
             :class="{'shadow' : selectedCard == card.id}"
             @click.native="selectedCard = card.id"
             v-for="card in cards"
             :card="card"
             :key="card.id"
-        ></Card>
+        ></app-card>
       </transition-group>
     </div>
     <div class="container">
-      <DefaultCard />
+      <transition name="rotate" mode="out-in">
+        <component
+            @click.native="showCard(answer)"
+            :is="activeCard"
+            :card="answer"
+        ></component>
+      </transition>
     </div>
   </div>
 </template>
@@ -23,25 +30,42 @@ import Card from './Card'
 import DefaultCard from './DefaultCard'
 export default {
   components : {
-    Card,
-    DefaultCard
+    appCard: Card,
+    appDefaultCard: DefaultCard
   },
   data(){
     return{
       selectedCard : null,
       answer : {},
+      activeCard: 'app-default-card',
       cards:[
-        {id: 1, component:'cards', image:'/src/assets/Braveheart.jpg'},
-        {id: 2, component:'cards', image:'/src/assets/captainFantastic.jpg'},
-        {id: 3, component:'cards', image:'/src/assets/forrestGump.jpg'},
-        {id: 4, component:'cards', image:'/src/assets/spiritedAway.jpg'},
-        {id: 5, component:'cards', image:'/src/assets/wall_e.jpg'}
+        {id: 1, component:'app-card', image:'/src/assets/Braveheart.jpg'},
+        {id: 2, component:'app-card', image:'/src/assets/captainFantastic.jpg'},
+        {id: 3, component:'app-card', image:'/src/assets/forrestGump.jpg'},
+        {id: 4, component:'app-card', image:'/src/assets/spiritedAway.jpg'},
+        {id: 5, component:'app-card', image:'/src/assets/wall_e.jpg'}
       ]
     }
   },
   created() {
     let answer = Math.ceil(Math.random() * this.cards.length);
     this.answer = this.cards[answer - 1]
+  },
+  methods : {
+    showCard(answer){
+      if (this.selectedCard == null){
+        alert('First, you should select a card');
+      }else{
+        this.activeCard = answer.component;
+        setTimeout(()=>{
+          if (answer.id == this.selectedCard){
+            alert('Correct!')
+          }else{
+            alert('Game is over!')
+          }
+        },1000)
+      }
+    }
   }
 }
 </script>
@@ -87,4 +111,31 @@ export default {
       transform: rotateY(360deg);
     }
   }
+
+  .rotate-enter{}
+  .rotate-enter-active{
+    animation: rotate-in ease-in-out 0.7s forwards;
+  }
+  .rotate-leave{}
+  .rotate-leave-active{
+    animation: rotate-out ease-in-out 0.7s forwards;
+  }
+
+  @keyframes rotate-in {
+    from{
+      transform: rotateY(90deg);
+    }
+    to{
+      transform: rotateY(0deg);
+    }
+  }
+  @keyframes rotate-out {
+    from{
+      transform: rotateY(0deg);
+    }
+    to{
+      transform: rotateY(90deg);
+    }
+  }
+
 </style>
